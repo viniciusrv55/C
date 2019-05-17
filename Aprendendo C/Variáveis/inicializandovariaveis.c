b7a871de575ff5c8764aa657117575f8a461b6ef
viniciusrv55 <42968892+viniciusrv55@users.noreply.github.com>
2019-05-16 21:24:59 -0300

    Create inicializandovariaveis.c

---
+++ Aprendendo C/Variáveis/inicializandovariaveis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    //Declaraçãod das variaveis
+    int numero = 46;
+    float valor = 8.37;
+    char letra = 'X';
+
+    printf("Variavel numero....: %d\n",numero);
+    printf("Variavel valor.....: %.2f\n",valor);
+    printf("Variavel letra.....: %c\n",letra);
+
+return 0;}
